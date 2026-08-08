4debe39a3e28d53318dd151ff2a512a0f5265b61
vedanthchamala <vedhuchamala@gmail.com>
2026-08-08 15:14:44 -0500

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pres = defaultdict(list)
+        finished = set()
+        path = set()
+        for course, pre in prerequisites:
+            pres[course].append(pre)
+        
+        def dfs(cur):
+            if cur in finished:
+                return True
+            if cur in path:
+                return False
+            path.add(cur)
+            for pre in pres[cur]:
+                if not dfs(pre):
+                    return False
+            path.remove(cur)
+            finished.add(cur)
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+
+            
+        
